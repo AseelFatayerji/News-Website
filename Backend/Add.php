339ccd597c0c9954eecd41b67afc2782d31c0bcf
Nyx-Nightmare <assel.fatayerji201@gmail.com>
2024-03-17 12:02:50 +0200

    Added image dispaly

--- Backend/Add.php
+++ Backend/Add.php
@@ -7,7 +7,7 @@ $title = $_POST['title'];
 $content = $_POST['content'];
 $author = $_POST['author'];
 $query = $mysqli->prepare('INSERT INTO news (title,content,author,images) VALUES(?,?,?,?);');
-$query->bind_param('ssss', $title, $content, $author, $imgData);
+$query->bind_param('ssss', $title, $content, $author, $img);
 $query->execute();
 $response['status'] = "success";
 $response['message'] = "news $title was added successfully for";
--- Frontend/Scripts/DisplayNews.js
+++ Frontend/Scripts/DisplayNews.js
@@ -9,7 +9,7 @@ jQuery(document).ready(function ($) {
         $("#news").empty();
         $.each(news, function (index, value) {
           $("#news").append(
-            '<div class = "card"><img src="' +
+            '<div class = "card"><img class="article-img" src="' +
               value["img"] +
               '"/> <div class="card-header">' +
               value["title"] +
