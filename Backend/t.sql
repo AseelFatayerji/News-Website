9294f9b4a359e40904c59f6b748636a31e48fdad
Nyx-Nightmare <assel.fatayerji201@gmail.com>
2024-03-18 13:49:14 +0200

    Fixed sql typo

--- Backend/t.sql
+++ Backend/t.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3308
--- Generation Time: Mar 17, 2024 at 10:10 AM
+-- Generation Time: Mar 18, 2024 at 11:47 AM
 -- Server version: 8.0.18
 -- PHP Version: 7.3.12
 
@@ -34,17 +34,17 @@ CREATE TABLE IF NOT EXISTS `news` (
   `title` varchar(25) DEFAULT NULL,
   `content` text,
   `author` varchar(25) DEFAULT NULL,
-  `images` longblob NOT NULL,
+  `images` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `news`
 --
 
 INSERT INTO `news` (`id`, `title`, `content`, `author`, `images`) VALUES
-(13, 'Student Moral', 'Student moral is on an all time low due to recent events ', 'Jane Doe', 0x706578656c732d616e647265612d7069616371756164696f2d333736303039362e6a7067),
-(14, 'Student Moral', 'Student moral is on an all time low due to recent events ', 'Jane Doe', 0x706578656c732d616e647265612d7069616371756164696f2d333736303039362e6a7067);
+(15, 'Importance of  Sleep', 'Study done on over 3000 students has shown a drastic decrease in sleep which has led to increased stress and depression, researchers compel educational facilities to lesson the load to allow students to sleep.', 'Sam Smith', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_mf5exQ9NDEO1ffRMtSYbZpeNF31zEqJoyTC3c0VNPA&s'),
+(14, 'Student Moral', 'Student moral is on an all time low due to recent events ', 'Jane Doe', 'https://img.freepik.com/free-photo/exhausted-man-sitting-desktop_23-2147655882.jpg?w=740&t=st=1710761899~exp=1710762499~hmac=1d385bbabfcb229c00e159148fa842c1f1f53ab693778ddef40f8bc49d0d776f');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
